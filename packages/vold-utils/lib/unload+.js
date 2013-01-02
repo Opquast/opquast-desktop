862f4fa7cf4f1ce1ccec72aed203b7475dd799e6
Olivier Meunier <olivier.meunier@temesis.com>
2013-01-02 11:24:17 +0100

    Updated layout for SDK 1.12.

--- packages/vold-utils/lib/unload+.js
+++ packages/vold-utils/lib/unload+.js
@@ -3,8 +3,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
-const { Class } = require("heritage");
-const unloadNS = require("namespace").ns();
+const { Class } = require("sdk/core/heritage");
+const unloadNS = require("sdk/core/namespace").ns();
 
 var Unloader = exports.Unloader = Class({
   initialize: function Unloader() {
